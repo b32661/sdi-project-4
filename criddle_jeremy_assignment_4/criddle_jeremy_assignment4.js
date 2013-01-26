013afffba30d8f38c7fc40108c32a8418a7ca8d8
Jeremy Criddle <jeremycriddle@fullsail.edu>
2013-01-26 05:50:46 -0500

    starting on functions

--- criddle_jeremy_assignment_4/criddle_jeremy_assignment4.js
+++ criddle_jeremy_assignment_4/criddle_jeremy_assignment4.js
@@ -1,1 +1,62 @@
-//alert("JavaScript works!");
+//  String	Number	Array
+//  Does a string follow a 123-456-7890 pattern like a phone number?
+//  Does a string follow an aaa@bbb.ccc pattern like an email address?
+//  Is the string a URL? (Does it start with http: or https:?)
+//  Title-case a string (split into words, then uppercase the first letter of each word)
+//  Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c".
+//  Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
+//  Fuzzy-match a number: is the number above or below a number within a certain percent?
+//  Find the number of hours or days difference between two dates.
+//  Given a string version of a number such as "42", return the value as an actual Number, such as 42.
+//  Find the smallest value in an array that is greater than a given number
+//  Find the total value of just the numbers in an array, even if some of the items are not numbers.
+//  Given an array of objects and the name of a key, return the array sorted by the value of that key in each of the objects: "a" + [{a:2},{a:3},{a:1}] → [{a:1},{a:2},{a:3}].
+
+
+// string phone number function
+function isPhoneNumber (string){
+    var phNum = string;
+
+}
+
+// string email address function
+    function isEmail (string){
+        var email = string;
+    }
+
+// string url function
+function isURL (string){
+    var url = string;
+}
+
+// title case a string function
+function makeCap (string){
+    var str = string;
+}
+
+// replace separator function
+function replaceSep (string){
+    var str = string;
+}
+
+// format a number for money function
+function makeMoney (number){
+    var num = number;
+}
+
+// fuzzy-match function
+function fuzMatch (number)
+
+// date difference function
+
+
+// string to actual number function
+
+
+// smallest value greater that a given number function
+
+
+// total value of just numbers in an array function
+
+
+// sorted array function
