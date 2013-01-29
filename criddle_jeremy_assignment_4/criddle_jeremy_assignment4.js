1be53b8f1cb36f55f9a11a2536d68066424d0ef7
Jeremy Criddle <jeremycriddle@fullsail.edu>
2013-01-29 06:49:02 -0500

    two working correctly

--- criddle_jeremy_assignment_4/criddle_jeremy_assignment4.js
+++ criddle_jeremy_assignment_4/criddle_jeremy_assignment4.js
@@ -1,18 +1,4 @@
 //  Does a string follow a 123-456-7890 pattern like a phone number?
-//  Does a string follow an aaa@bbb.ccc pattern like an email address?
-//  Is the string a URL? (Does it start with http: or https:?)
-//  Title-case a string (split into words, then uppercase the first letter of each word)
-//  Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c".
-//  Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
-//  Fuzzy-match a number: is the number above or below a number within a certain percent?
-//  Find the number of hours or days difference between two dates.
-//  Given a string version of a number such as "42", return the value as an actual Number, such as 42.
-//  Find the smallest value in an array that is greater than a given number
-//  Find the total value of just the numbers in an array, even if some of the items are not numbers.
-//  Given an array of objects and the name of a key, return the array sorted by the value of that key in each of the objects: "a" + [{a:2},{a:3},{a:1}] → [{a:1},{a:2},{a:3}].
-
-
-// string phone number function
 function isPhoneNumber (string){
     var phNum = string;
     var pLength = phNum.length;
@@ -37,11 +23,10 @@ function isPhoneNumber (string){
     return phNum;
 }
 
-// string email address function
+//  Does a string follow an aaa@bbb.ccc pattern like an email address?
 function isEmail (string){
     var email = string;
     var myEmail;
-    var firstThree = email.substring(0,3);
     var lastThree = email.substring(email.length-3, email.length);
     var dot = email.charAt(email.length - 4);
     var firstAt = email.indexOf("@");
@@ -49,24 +34,32 @@ function isEmail (string){
     var firstDot = email.indexOf(".");
     var lastDot = email.lastIndexOf(".");
 
-    if( dot != "." || firstAt == -1 ||
-        firstDot != lastDot || firstAt != secondAt
-        //email.charAt(email.length) != NaN ||
-        //email.charAt(email.length-1) != NaN ||
-        //email.charAt(email.length-2) != NaN ||
-       // email.charAt(0) != NaN ||
-        //email.charAt(0) != NaN ||
-        ){
-        var myEmail = false;
+    if( dot === "." && firstAt != -1 &&
+        firstDot === lastDot &&
+        firstAt === secondAt )
+        {
+            if(lastThree === "com" ||
+                lastThree ==="net" ||
+                lastThree === "org"){
+
+            } else{
+                myEmail = false;
+                console.log("This is probably not an email address.");
+                return myEmail
+            }
+
+        var myEmail = true;
+            console.log("This is possibly an email address.");
 
     } else {
-        myEmail = true;
+        myEmail = false;
+        console.log("I doubt this is an email address.")
 }
 
-    return {isEmail :myEmail, email: email};
+    return myEmail
 }
 
-// string url function
+//  Is the string a URL? (Does it start with http: or https:?)
 function isURL (string){
     var url = string;
     var firstFive = url.substring(0,5);
@@ -81,52 +74,57 @@ function isURL (string){
     return {string: url ,first: firstFive, second: firstSix, possibleURL: isUrl}
 }
 
-// title case a string function
+//  Title-case a string (split into words, then uppercase the first letter of each word)
 function makeCap (string){
     var str = string;
     return str;
 }
 
-// replace separator function
+//  Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c".
 function replaceSep (string){
     var str = string;
 }
 
-// format a number for money function
+//  Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
 function makeMoney (number){
     var num = number;
 }
 
-// fuzzy-match function
-function fuzMatch (number)
+//  Fuzzy-match a number: is the number above or below a number within a certain percent?
+function fuzMatch (number){
 
-// date difference function
+}
+
+//  Find the number of hours or days difference between two dates.
 function dateDif (date){
-    var myDate = date;
+    var userDate = date;
+    var now =  new Date();
+    result = userDate - now;
+    return result;
 }
 
-// string to actual number function
+//  Given a string version of a number such as "42", return the value as an actual Number, such as 42.
 function strToNum (string){
-    var toNum = string;
+    var number = string;
+
 }
 
-// smallest value greater that a given number function
+//  Find the smallest value in an array that is greater than a given number
 function grThan (number){
     var myNumber = number
 }
 
-// total value of just numbers in an array function
+//  Find the total value of just the numbers in an array, even if some of the items are not numbers.
 function valNumInArray (array){
     var myArray = array;
 }
 
-// sorted array function
+
+//  Given an array of objects and the name of a key, return the array sorted by the value of that key in each of the objects: "a" + [{a:2},{a:3},{a:1}] → [{a:1},{a:2},{a:3}].
 function sortArray (array){
     var myArray = array;
 }
 
-var well = isEmail("jermcriddle@gmail.com");
+var well = isEmail("jermcriddle@gmail.net");
 console.log(well);
-var net = isURL("http:jarees");
-console.log(net);
 
