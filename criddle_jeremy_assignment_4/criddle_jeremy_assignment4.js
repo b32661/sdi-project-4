7a67edf07ff1a7e5d5cabe632d98edc40ed1877d
Jeremy Criddle <jeremycriddle@fullsail.edu>
2013-01-31 15:54:06 -0500

    toUpper Function

--- criddle_jeremy_assignment_4/criddle_jeremy_assignment4.js
+++ criddle_jeremy_assignment_4/criddle_jeremy_assignment4.js
@@ -3,24 +3,24 @@
 //Project 4
 //Function Library
 
-var JerLib = function(){
+var JerLib = function () {
 
 
 //  Does a string follow a 123-456-7890 pattern like a phone number? returns true or false.
-    var isPhoneNumber = function (string){
+    var isPhoneNumber = function (string) {
         var phNum = string;
         var pLength = phNum.length;
         var pFirst = phNum.charAt(0);
-        var pFirstThree = phNum.substring(0,2);
-        var pSecondThree = phNum.substring((4,6));
-        var pLastFour = phNum.substring(8,11);
+        var pFirstThree = phNum.substring(0, 2);
+        var pSecondThree = phNum.substring((4, 6));
+        var pLastFour = phNum.substring(8, 11);
         var pFirstDash = phNum.charAt(3);
         var pLastDash = phNum.charAt(7);
         var dashes = pFirstDash + pLastDash;
 
         if (pLength != 12 || pFirst === 0 ||
             pFirstThree === NaN || pSecondThree === NaN ||
-            pLastFour === NaN || dashes != "--"){
+            pLastFour === NaN || dashes != "--") {
             phNum = false;
         } else {
             phNum = true;
@@ -31,85 +31,85 @@ var JerLib = function(){
 
 
 //  Does a string follow an aaa@bbb.ccc pattern like an email address? returns true or false
-     var isEmail = function (string){
+    var isEmail = function (string) {
         var email = string;
         var myEmail;
-        var lastThree = email.substring(email.length-3, email.length);
+        var lastThree = email.substring(email.length - 3, email.length);
         var dot = email.charAt(email.length - 4);
         var firstAt = email.indexOf("@");
         var secondAt = email.lastIndexOf("@");
         var firstDot = email.indexOf(".");
         var lastDot = email.lastIndexOf(".");
 
-     if( dot === "." && firstAt != -1 &&
-         firstDot === lastDot &&
-         firstAt === secondAt )
-          {
-             if(lastThree === "com" ||
-                    lastThree ==="net" ||
-                    lastThree === "org"){
-                 myEmail= true;
+        if (dot === "." && firstAt != -1 &&
+            firstDot === lastDot &&
+            firstAt === secondAt) {
+            if (lastThree === "com" ||
+                lastThree === "net" ||
+                lastThree === "org") {
+                myEmail = true;
 
-             } else{
-                    myEmail = false;
-                    return myEmail;
-             }
+            } else {
+                myEmail = false;
+                return myEmail;
+            }
 
-      myEmail = true;
+            myEmail = true;
 
-     } else {
-        myEmail = false;
-}
+        } else {
+            myEmail = false;
+        }
 
-     return myEmail;
-}
+        return myEmail;
+    }
 
 
 //  Is the string a URL? (Does it start with http: or https:?) returns true or false
-    var isUrL = function (string){
+    var isUrL = function (string) {
         var url = string;
-        var firstFive = url.substring(0,5);
-        var firstSix = url.substring(0,6);
+        var firstFive = url.substring(0, 5);
+        var firstSix = url.substring(0, 6);
         var isUrl;
 
-        if (firstFive == "http:" || firstSix == "https:"){
-          isUrl = true ;
+        if (firstFive == "http:" || firstSix == "https:") {
+            isUrl = true;
             return isUrl;
         } else {
             isUrl = false;
             return isUrl;
-     }
+        }
         ;
     }
 
 
-//  Title-case a string (split into words, then uppercase the first letter of each word)
-    var makeCap = function (string){
-    var split = str.split(" ");
-    var result = "";
-    var cased;
-    for (var i = 0, w = split.length; i < w; i++) {
-        var spNew = split[i].replace(split[i].charAt(0),(split[i].charAt(0)).toUpperCase());
-        cased = result.concat(spNew + " ");
-    };
-    return cased;
-};
-//    }
+    var toUpper = function (string) {
+        var splitter = string.split(' ');
+        var sub, substr ;
+        var word = new Array();
+        for (i = 0; i < splitter.length; i++) {
+            sub = splitter[i].substring(0, 1).toUpperCase();
+            substr = splitter[i].substring(1);
+            word[i] = sub + substr;
+        }
+        var capsString = word.join(' ');
+        return capsString;
+    }
+
 
 
 //  Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c".
     var replaceSep = function (string, separator) {
-        var re = /\W/g;
-        var replaced = str.replace(re, newSep);
+        var str = /\W/g;
+        var replaced = str.replace(string, separator);
         return replaced;
-//    }
+    };
 
 
 //  Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
         var makeMoney = function (number) {
             var num = number.toFixed(2);
             return num;
-        }
+        };
 
 
 //  Fuzzy-match a number: is the number above or below a number within a certain percent?
@@ -129,10 +129,10 @@ var JerLib = function(){
 // Input two dates using new Date(yyyy,mm,dd)
 // Output format is hours
         var difInHours = function (date1,date2) {
-        var dif ;
-        var difference = (date1 > date2) ? date1.getTime() - date2.getTime() : date2.getTime() - date1.getTime();
-        dif = difference / 1000 * 60 * 60;
-        return dif;
+            var dif ;
+            var difference = (date1 > date2) ? date1.getTime() - date2.getTime() : date2.getTime() - date1.getTime();
+            dif = difference / 1000 * 60 * 60;
+            return dif;
         }
 
 
@@ -161,16 +161,17 @@ var JerLib = function(){
 //    }
 
 
-        return {"phNum": phNum,
-            "isAnEmail": myEmail,
-            "isaURL": isUrl,
-            "titleCased": cased,
-            "replaced": replaced,
-            "num": num,
-            "str": str,
-            "dif": dif,
-            "isFuzzyNum": isFuzzyNum,
+        return {"phNum":phNum,
+            "isAnEmail":myEmail,
+            "isaURL":isUrl,
+            "titleCased":cased,
+            "replaced":replaced,
+            "num":num,
+            "str":str,
+            "dif":dif,
+            "isFuzzyNum":isFuzzyNum,
+            "capsString": capString
 
-        };
-    };
-}
+
+
+        }     };
