c136f1f97a541462334019a137a560352e660b91
Jeremy Criddle <jeremycriddle@fullsail.edu>
2013-01-31 19:42:52 -0500

    date dif function

--- criddle_jeremy_assignment_4/criddle_jeremy_assignment4.js
+++ criddle_jeremy_assignment_4/criddle_jeremy_assignment4.js
@@ -1,39 +1,41 @@
-//Jeremy Criddle
-//SDI 1301
-//Project 4
-//Function Library
+//  Jeremy Criddle
+//  SDI 1301
+//  Project 4
+//  Function Library
 
-var JerLib = function () {
+var jerLib = function() {
 
 
 //  Does a string follow a 123-456-7890 pattern like a phone number? returns true or false.
-    var isPhoneNumber = function (string) {
-        var phNum = string;
+    var isPhoneNumber = function(string){
+        var phNum = string ;
         var pLength = phNum.length;
         var pFirst = phNum.charAt(0);
         var pFirstThree = phNum.substring(0, 2);
-        var pSecondThree = phNum.substring((4, 6));
+        pFirstThree = pFirstThree - 0;
+        var pSecondThree = phNum.substring(4, 6);
+        pSecondThree = pSecondThree - 0;
         var pLastFour = phNum.substring(8, 11);
+        pLastFour = pLastFour - 0;
         var pFirstDash = phNum.charAt(3);
         var pLastDash = phNum.charAt(7);
         var dashes = pFirstDash + pLastDash;
 
+
         if (pLength != 12 || pFirst === 0 ||
-            pFirstThree === NaN || pSecondThree === NaN ||
-            pLastFour === NaN || dashes != "--") {
-            phNum = false;
+            isNaN(pFirstThree) || isNaN(pSecondThree) ||
+            isNaN (pLastFour)  || dashes != "--") {
+            return false;
         } else {
-            phNum = true;
+            return true;
         }
 
-        return phNum;
     }
 
 
 //  Does a string follow an aaa@bbb.ccc pattern like an email address? returns true or false
     var isEmail = function (string) {
         var email = string;
-        var myEmail;
         var lastThree = email.substring(email.length - 3, email.length);
         var dot = email.charAt(email.length - 4);
         var firstAt = email.indexOf("@");
@@ -47,20 +49,14 @@ var JerLib = function () {
             if (lastThree === "com" ||
                 lastThree === "net" ||
                 lastThree === "org") {
-                myEmail = true;
-
-            } else {
-                myEmail = false;
-                return myEmail;
-            }
-
-            myEmail = true;
-
-        } else {
-            myEmail = false;
+                return true;
+                } else {
+                    return false;
+                    }
+            return true;
+                } else {
+                return false;
         }
-
-        return myEmail;
     }
 
 
@@ -69,20 +65,18 @@ var JerLib = function () {
         var url = string;
         var firstFive = url.substring(0, 5);
         var firstSix = url.substring(0, 6);
-        var isUrl;
-
         if (firstFive == "http:" || firstSix == "https:") {
-            isUrl = true;
-            return isUrl;
+            return true;
         } else {
-            isUrl = false;
-            return isUrl;
+            return false;
+
         }
         ;
     }
 
 
-    var toUpper = function (string) {
+    //Title-case a string (split into words, then uppercase the first letter of each word)
+    var toTitle = function (string) {
         var splitter = string.split(' ');
         var sub, substr ;
         var word = new Array();
@@ -91,18 +85,17 @@ var JerLib = function () {
             substr = splitter[i].substring(1);
             word[i] = sub + substr;
         }
-        var capsString = word.join(' ');
-        return capsString;
+        return word;
     }
 
 
 
 //  Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c".
-    var replaceSep = function (string, separator) {
-        var str = /\W/g;
-        var replaced = str.replace(string, separator);
-        return replaced;
-    };
+    var replaceSep = function (string, separator, toSeparator) {
+            var arrayOfStrings = string.split(separator);
+            replaced = arrayOfStrings.join(toSeparator)
+            return replaced;
+        };
 
 
 //  Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
@@ -126,13 +119,14 @@ var JerLib = function () {
         };
 
 
-// Input two dates using new Date(yyyy,mm,dd)
+// Input two dates, small number first using new Date(yyyy,mm,dd)
 // Output format is hours
-        var difInHours = function (date1,date2) {
-            var dif ;
-            var difference = (date1 > date2) ? date1.getTime() - date2.getTime() : date2.getTime() - date1.getTime();
-            dif = difference / 1000 * 60 * 60;
-            return dif;
+        var difInHours = function (date1, date2) {
+            var firstDate = date1;
+            var secondDate = date2;
+            var difference = (firstDate > secondDate) ? firstDate.getTime() - secondDate.getTime() : secondDate.getTime() - firstDate.getTime();
+            return difference / 1000 * 60 * 60  ;
+
         }
 
 
@@ -161,17 +155,21 @@ var JerLib = function () {
 //    }
 
 
-        return {"phNum":phNum,
-            "isAnEmail":myEmail,
-            "isaURL":isUrl,
-            "titleCased":cased,
-            "replaced":replaced,
-            "num":num,
-            "str":str,
-            "dif":dif,
-            "isFuzzyNum":isFuzzyNum,
-            "capsString": capString
-
-
-
+        return {"isPhoneNumber": isPhoneNumber,
+            "isEmail":isEmail,
+            "isUrl":isUrL,
+            "toTitle":toTitle,
+            "replaceSep":replaceSep,
+            "makeMoney":makeMoney,
+            "difInHours":difInHours,
+            "strToNum": strToNum,
+            "fuzzyNum":fuzzyNum
         }     };
+
+var newLib = new jerLib();
+console.log(newLib.isPhoneNumber("256-655-0016"));
+console.log(newLib.isEmail("jermcriddle@gmail.com"));
+console.log(newLib.isUrl("http:"));
+console.log(newLib.replaceSep("today-is-the-day","-", "="));
+console.log(newLib.difInHours(new Date(2011,11,11),new Date(2012,11,11)));
+console.log(newLib.toTitle("today is the day"));
